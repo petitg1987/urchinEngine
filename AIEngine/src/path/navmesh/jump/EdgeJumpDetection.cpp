5c056f9cceb78934b2d83ee17d98dc313c402261
greg <petitg1987@gmail.com>
2019-11-25 09:43:58 +0100

    AI: fix edge jump detection algorithm

--- AIEngine/src/path/navmesh/jump/EdgeJumpDetection.cpp
+++ AIEngine/src/path/navmesh/jump/EdgeJumpDetection.cpp
@@ -11,7 +11,7 @@ namespace urchin
      * See '_doc' to understand formulas
      */
     void EdgeJumpDetection::detectJumpFromEdge1ToEdge2(const LineSegment3D<float> &edge1, const LineSegment3D<float> &edge2, float jumpMaxLength) const
-    {
+    { //TODO rename method + handle division by 0
         Vector3<float> edge1Vector = edge1.getA().vector(edge1.getB());
         Vector3<float> cCommonFraction = ( edge1Vector * edge1Vector ) / (edge1Vector.squareLength());
         Vector3<float> c0 = edge2.getB().vector(edge2.getA()) - (cCommonFraction * (edge2.getB().vector(edge2.getA())));
@@ -22,11 +22,13 @@ namespace urchin
         float quadraticC = c1.X*c1.X + c1.Y*c1.Y + c1.Z*c1.Z - jumpMaxLength*jumpMaxLength;
         float quadraticSqrt = std::sqrt(quadraticB*quadraticB - 4.0f * quadraticA * quadraticC);
 
-        float r1 = std::clamp((-quadraticB + quadraticSqrt) / (2.0f * quadraticA), 0.0f, 1.0f);
-        float r2 = std::clamp((-quadraticB - quadraticSqrt) / (2.0f * quadraticA), 0.0f, 1.0f);
+        float r1 = (-quadraticB + quadraticSqrt) / (2.0f * quadraticA);
+        float r2 = (-quadraticB - quadraticSqrt) / (2.0f * quadraticA);
+        float r1Clamped = std::clamp(r1, 0.0f, 1.0f);
+        float r2clamped = std::clamp(r2, 0.0f, 1.0f);
 
-        Point3<float> p1 = r1*edge2.getA() + (1-r1) * edge2.getB();
-        Point3<float> p2 = r2*edge2.getA() + (1-r2) * edge2.getB();
+        Point3<float> p1 = r1Clamped*edge2.getA() + (1.0f - r1Clamped) * edge2.getB();
+        Point3<float> p2 = r2clamped*edge2.getA() + (1.0f - r2clamped) * edge2.getB();
 
         std::cout<<std::endl<<"Point 1: "<<p1<<", point 2: "<<p2<<std::endl;
     }
