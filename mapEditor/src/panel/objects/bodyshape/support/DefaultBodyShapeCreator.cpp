34e3f584590c2f6b53a342b61d61d50b72bd0ca5
greg <petitg1987@gmail.com>
2021-07-14 13:58:38 +0200

    Use unique_ptr instead of new/delete

--- mapEditor/src/panel/objects/bodyshape/support/DefaultBodyShapeCreator.cpp
+++ mapEditor/src/panel/objects/bodyshape/support/DefaultBodyShapeCreator.cpp
@@ -8,31 +8,31 @@ namespace urchin {
     }
 
     std::unique_ptr<const CollisionShape3D> DefaultBodyShapeCreator::createDefaultBodyShape(CollisionShape3D::ShapeType shapeType) const {
-        CollisionShape3D *shape;
+        std::unique_ptr<CollisionShape3D> shape;
         const AABBox<float>& modelAABBox = sceneObject.getModel()->getLocalAABBox();
 
         if (shapeType == CollisionShape3D::ShapeType::BOX_SHAPE) {
-            shape = new CollisionBoxShape(modelAABBox.getHalfSizes());
+            shape = std::make_unique<CollisionBoxShape>(modelAABBox.getHalfSizes());
         } else if (shapeType == CollisionShape3D::ShapeType::SPHERE_SHAPE) {
-            shape = new CollisionSphereShape(modelAABBox.getMaxHalfSize());
+            shape = std::make_unique<CollisionSphereShape>(modelAABBox.getMaxHalfSize());
         } else if (shapeType == CollisionShape3D::ShapeType::CAPSULE_SHAPE) {
             float radius = std::max(modelAABBox.getHalfSizes()[1],  modelAABBox.getHalfSizes()[2]);
             float cylinderHeight = modelAABBox.getHalfSizes()[0]*2.0f;
 
-            shape = new CollisionCapsuleShape(radius, cylinderHeight, CapsuleShape<float>::CAPSULE_X);
+            shape = std::make_unique<CollisionCapsuleShape>(radius, cylinderHeight, CapsuleShape<float>::CAPSULE_X);
         } else if (shapeType == CollisionShape3D::ShapeType::CYLINDER_SHAPE) {
             float radius = std::max(modelAABBox.getHalfSizes()[1],  modelAABBox.getHalfSizes()[2]);
             float height = modelAABBox.getHalfSizes()[0]*2.0f;
 
-            shape = new CollisionCylinderShape(radius, height, CylinderShape<float>::CYLINDER_X);
+            shape = std::make_unique<CollisionCylinderShape>(radius, height, CylinderShape<float>::CYLINDER_X);
         } else if (shapeType == CollisionShape3D::ShapeType::CONE_SHAPE) {
             float radius = std::max(modelAABBox.getHalfSizes()[1],  modelAABBox.getHalfSizes()[2]);
             float height = modelAABBox.getHalfSizes()[0]*2.0f;
 
-            shape = new CollisionConeShape(radius, height, ConeShape<float>::CONE_X_POSITIVE);
+            shape = std::make_unique<CollisionConeShape>(radius, height, ConeShape<float>::CONE_X_POSITIVE);
         } else if (shapeType == CollisionShape3D::ShapeType::CONVEX_HULL_SHAPE) {
             auto convexHullShape = buildConvexHullShape(sceneObject.getModel());
-            shape = new CollisionConvexHullShape(std::move(convexHullShape));
+            shape = std::make_unique<CollisionConvexHullShape>(std::move(convexHullShape));
         } else if (shapeType == CollisionShape3D::ShapeType::COMPOUND_SHAPE) {
             std::vector<std::shared_ptr<const LocalizedCollisionShape>> localizedCollisionShapes;
 
@@ -42,16 +42,13 @@ namespace urchin {
             boxLocalizedShape->transform = PhysicsTransform();
             localizedCollisionShapes.push_back(std::move(boxLocalizedShape));
 
-            shape = new CollisionCompoundShape(std::move(localizedCollisionShapes));
+            shape = std::make_unique<CollisionCompoundShape>(std::move(localizedCollisionShapes));
         } else {
             throw std::invalid_argument("Unknown shape type to create default body shape: " + std::to_string(shapeType));
         }
 
         float scale = sceneObject.getModel()->getTransform().getScale();
-        std::unique_ptr<const CollisionShape3D> scaledShape = shape->scale(scale);
-        delete shape;
-
-        return scaledShape;
+        return shape->scale(scale);
     }
 
     std::unique_ptr<ConvexHullShape3D<float>> DefaultBodyShapeCreator::buildConvexHullShape(const Model* model) const {
