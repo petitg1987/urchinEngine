4adcf508c89a5f883e0d1d8e8ea02134d25a1b10
greg <petitg1987@gmail.com>
2021-09-07 18:43:24 +0200

    Minor fix

--- 3dEngine/src/loader/font/LoaderTTF.cpp
+++ 3dEngine/src/loader/font/LoaderTTF.cpp
@@ -120,7 +120,7 @@ namespace urchin {
 
         auto alphabetTexture = Image(dimensionTexture, dimensionTexture, Image::IMAGE_RGBA, std::move(texels), true).createTexture(false);
 
-        //clears buffers of letters
+        //clear buffers of letters
         for (std::size_t i = 0; i < Font::NUM_LETTERS; i++) {
             glyph[i].buf.clear();
         }
