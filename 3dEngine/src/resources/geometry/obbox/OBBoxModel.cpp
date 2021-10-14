477c958e8394c65383cbb1538c26f3e3e4165d84
greg <petitg1987@gmail.com>
2021-10-14 11:32:02 +0200

    Use indices for OBB model

--- 3dEngine/src/resources/geometry/obbox/OBBoxModel.cpp
+++ 3dEngine/src/resources/geometry/obbox/OBBoxModel.cpp
@@ -9,63 +9,68 @@ namespace urchin {
 
     }
 
-    std::vector<Point3<float>> OBBoxModel::retrieveVertexArray(std::vector<uint32_t>&) const {
+    std::vector<Point3<float>> OBBoxModel::retrieveVertexArray(std::vector<uint32_t>& indices) const {
+        indices.reserve(6 * 6);
         std::vector<Point3<float>> vertexArray;
-        vertexArray.reserve(6 * 6);
+        vertexArray.reserve(8);
 
+        for(unsigned int i = 0; i < 8; ++i) {
+            vertexArray.push_back(obbox.getPoint(i));
+        }
+        
         //back
-        vertexArray.push_back(obbox.getPoint(7));
-        vertexArray.push_back(obbox.getPoint(3));
-        vertexArray.push_back(obbox.getPoint(5));
+        indices.push_back(7);
+        indices.push_back(3);
+        indices.push_back(5);
 
-        vertexArray.push_back(obbox.getPoint(5));
-        vertexArray.push_back(obbox.getPoint(3));
-        vertexArray.push_back(obbox.getPoint(1));
+        indices.push_back(5);
+        indices.push_back(3);
+        indices.push_back(1);
 
         //front
-        vertexArray.push_back(obbox.getPoint(2));
-        vertexArray.push_back(obbox.getPoint(6));
-        vertexArray.push_back(obbox.getPoint(4));
+        indices.push_back(2);
+        indices.push_back(6);
+        indices.push_back(4);
 
-        vertexArray.push_back(obbox.getPoint(4));
-        vertexArray.push_back(obbox.getPoint(0));
-        vertexArray.push_back(obbox.getPoint(2));
+        indices.push_back(4);
+        indices.push_back(0);
+        indices.push_back(2);
 
         //left
-        vertexArray.push_back(obbox.getPoint(4));
-        vertexArray.push_back(obbox.getPoint(6));
-        vertexArray.push_back(obbox.getPoint(7));
+        indices.push_back(4);
+        indices.push_back(6);
+        indices.push_back(7);
 
-        vertexArray.push_back(obbox.getPoint(7));
-        vertexArray.push_back(obbox.getPoint(5));
-        vertexArray.push_back(obbox.getPoint(4));
+        indices.push_back(7);
+        indices.push_back(5);
+        indices.push_back(4);
 
         //right
-        vertexArray.push_back(obbox.getPoint(2));
-        vertexArray.push_back(obbox.getPoint(0));
-        vertexArray.push_back(obbox.getPoint(3));
+        indices.push_back(2);
+        indices.push_back(0);
+        indices.push_back(3);
 
-        vertexArray.push_back(obbox.getPoint(3));
-        vertexArray.push_back(obbox.getPoint(0));
-        vertexArray.push_back(obbox.getPoint(1));
+        indices.push_back(3);
+        indices.push_back(0);
+        indices.push_back(1);
 
         //bottom
-        vertexArray.push_back(obbox.getPoint(7));
-        vertexArray.push_back(obbox.getPoint(6));
-        vertexArray.push_back(obbox.getPoint(2));
+        indices.push_back(7);
+        indices.push_back(6);
+        indices.push_back(2);
 
-        vertexArray.push_back(obbox.getPoint(2));
-        vertexArray.push_back(obbox.getPoint(3));
-        vertexArray.push_back(obbox.getPoint(7));
+        indices.push_back(2);
+        indices.push_back(3);
+        indices.push_back(7);
 
         //top
-        vertexArray.push_back(obbox.getPoint(0));
-        vertexArray.push_back(obbox.getPoint(4));
-        vertexArray.push_back(obbox.getPoint(5));
+        indices.push_back(0);
+        indices.push_back(4);
+        indices.push_back(5);
 
-        vertexArray.push_back(obbox.getPoint(5));
-        vertexArray.push_back(obbox.getPoint(1));
-        vertexArray.push_back(obbox.getPoint(0));
+        indices.push_back(5);
+        indices.push_back(1);
+        indices.push_back(0);
 
         return vertexArray;
     }
