e1466911a1bd850a394b53149ac20881f197321f
petitg1987 <petitg1987@gmail.com>
2021-11-03 13:06:35 +0100

    Improve blender exporter deploy script

--- 3dEngine/resources/blender/deploy.sh
+++ 3dEngine/resources/blender/deploy.sh
@@ -5,8 +5,10 @@ cd "$(dirname "$0")"
 
 blenderVersion=$(blender --version | grep -oP '\d.\d+')
 targetPath="$HOME/.config/blender/$blenderVersion/scripts/addons/"
+mkdir -p $targetPath
 
 cp "./io_export_urchin.py" "$targetPath"
 
 echo "Script deployed into: $targetPath"
-echo "Press F3 in Blender, search for 'reload' and select 'Reload Scripts'"
+echo "[ONE SHOT] Enable exporter in Edit > Preferences... > Add-ons"
+echo "[EVERYTIME] Press F3 in Blender, search for 'reload' and select 'Reload Scripts'"
