c5036fad7698d824ba7ae1679484b5ebfd80c617
petitg1987 <petitg1987@gmail.com>
2022-05-19 10:24:36 +0200

    Uda parser: allow to remove chunks

--- test/src/common/io/uda/UdaParserTest.cpp
+++ test/src/common/io/uda/UdaParserTest.cpp
@@ -7,17 +7,20 @@ using namespace urchin;
 void UdaParserTest::removeChunk() {
     std::string filename = FileSystem::instance().getResourcesDirectory() + "test.uda";
     UdaParser udaParserWriter(filename, UdaLoadType::NO_LOAD);
-    UdaChunk& main = udaParserWriter.createChunk("main", UdaAttribute(), nullptr);
-    udaParserWriter.createChunk("child1", UdaAttribute(), &main).setStringValue("child1");
-    udaParserWriter.createChunk("child2", UdaAttribute(), &main).setStringValue("child2");
-    udaParserWriter.createChunk("child3", UdaAttribute(), &main).setStringValue("child3");
-    UdaChunk* child2Retrieved = udaParserWriter.getFirstChunk(true, "child2", UdaAttribute(), &main);
+    UdaChunk& newMain = udaParserWriter.createChunk("main", UdaAttribute(), nullptr);
+    udaParserWriter.createChunk("child1", UdaAttribute(), &newMain).setStringValue("child1");
+    udaParserWriter.createChunk("child2", UdaAttribute(), &newMain).setStringValue("child2");
+    udaParserWriter.createChunk("child3", UdaAttribute(), &newMain).setStringValue("child3");
+    UdaChunk* child2Retrieved = udaParserWriter.getFirstChunk(true, "child2", UdaAttribute(), &newMain);
 
     udaParserWriter.removeChunk(*child2Retrieved);
     udaParserWriter.save();
 
     UdaParser udaParserReader(filename, UdaLoadType::LOAD_FILE);
-    AssertHelper::assertObjectEquals(udaParserReader.getFirstChunk(false, "child2", UdaAttribute(), &main), nullptr);
+    UdaChunk* main = udaParserReader.getFirstChunk(true, "main", UdaAttribute(), nullptr);
+    udaParserReader.getFirstChunk(true, "child1", UdaAttribute(), main); //no exception throw expected
+    AssertHelper::assertObjectEquals(udaParserReader.getFirstChunk(false, "child2", UdaAttribute(), main), nullptr);
+    udaParserReader.getFirstChunk(true, "child3", UdaAttribute(), main); //no exception throw expected
     AssertHelper::assertIntEquals(std::remove(filename.c_str()), 0);
 }
 
