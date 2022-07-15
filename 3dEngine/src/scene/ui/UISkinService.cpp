fa3a2b9fe640114096b363a0f5fa108bc3d20b49
petitg1987 <petitg1987@gmail.com>
2022-07-16 00:10:30 +0200

    Improve performance to build 9-sliced widgets

--- 3dEngine/src/scene/ui/UISkinService.cpp
+++ 3dEngine/src/scene/ui/UISkinService.cpp
@@ -43,7 +43,11 @@ namespace urchin {
         }
 
         //creates the image width * height
-        unsigned int componentsCount = rawWidgetImage->retrieveComponentsCount(); //shortcut
+        unsigned int componentsCount = rawWidgetImage->retrieveComponentsCount();
+        const std::vector<unsigned char>& rawTexels = rawWidgetImage->getTexels();
+        unsigned int rawWidth = rawWidgetImage->getWidth();
+        unsigned int rawHeight = rawWidgetImage->getHeight();
+
         std::vector<unsigned char> texels(height * width * componentsCount, 0);
 
         auto widthMinusRight = (unsigned int)std::max(0, (int)width - (int)right);
@@ -54,63 +58,63 @@ namespace urchin {
         //copy corner top left
         for (unsigned int i = 0; i < topAdjusted; ++i) {
             for (unsigned int j = 0; j < leftMultiplyInternalFormat; ++j) {
-                texels[i * width * componentsCount + j] = rawWidgetImage->getTexels()[i * rawWidgetImage->getWidth() * componentsCount + j];
+                texels[i * width * componentsCount + j] = rawTexels[i * rawWidth * componentsCount + j];
             }
         }
 
         //copy top
         for (unsigned int i = 0; i < topAdjusted; ++i) {
             for (unsigned int j = left * componentsCount, k = 0; j < widthMinusRight * componentsCount; ++j, ++k) {
-                texels[i * width * componentsCount + j] = rawWidgetImage->getTexels()[i * rawWidgetImage->getWidth() * componentsCount + k % ((rawWidgetImage->getWidth() - (right + left)) * componentsCount) + left * componentsCount];
+                texels[i * width * componentsCount + j] = rawTexels[i * rawWidth * componentsCount + k % ((rawWidth - (right + left)) * componentsCount) + left * componentsCount];
             }
         }
 
         //copy corner top right
         for (unsigned int i = 0; i < topAdjusted; ++i) {
-            for (unsigned int j = widthMinusRight * componentsCount, k = (rawWidgetImage->getWidth()-right) * componentsCount; j < width * componentsCount; ++j, ++k) {
-                texels[i * width * componentsCount + j] = rawWidgetImage->getTexels()[i * rawWidgetImage->getWidth() * componentsCount + k];
+            for (unsigned int j = widthMinusRight * componentsCount, k = (rawWidth - right) * componentsCount; j < width * componentsCount; ++j, ++k) {
+                texels[i * width * componentsCount + j] = rawTexels[i * rawWidth * componentsCount + k];
             }
         }
 
         //copy right
         for (unsigned int i = top, k = 0; i < heightMinusBottom; ++i, ++k) {
-            for (unsigned int j = widthMinusRight * componentsCount, l = (rawWidgetImage->getWidth() - right) * componentsCount; j < width * componentsCount; ++j, ++l) {
-                texels[i * width * componentsCount + j] = rawWidgetImage->getTexels()[(k % (rawWidgetImage->getHeight() - (top + bottom)) + top) * rawWidgetImage->getWidth() * componentsCount + l];
+            for (unsigned int j = widthMinusRight * componentsCount, l = (rawWidth - right) * componentsCount; j < width * componentsCount; ++j, ++l) {
+                texels[i * width * componentsCount + j] = rawTexels[(k % (rawHeight - (top + bottom)) + top) * rawWidth * componentsCount + l];
             }
         }
 
         //copy corner bottom right
-        for (unsigned int i = heightMinusBottom, k = rawWidgetImage->getHeight() - bottom; i < height; ++i, ++k) {
-            for (unsigned int j = widthMinusRight * componentsCount, l = (rawWidgetImage->getWidth() - right) * componentsCount; j < width * componentsCount; ++j, ++l) {
-                texels[i * width * componentsCount + j] = rawWidgetImage->getTexels()[k * rawWidgetImage->getWidth() * componentsCount + l];
+        for (unsigned int i = heightMinusBottom, k = rawHeight - bottom; i < height; ++i, ++k) {
+            for (unsigned int j = widthMinusRight * componentsCount, l = (rawWidth - right) * componentsCount; j < width * componentsCount; ++j, ++l) {
+                texels[i * width * componentsCount + j] = rawTexels[k * rawWidth * componentsCount + l];
             }
         }
 
         //copy bottom
-        for (unsigned int i = heightMinusBottom, k = rawWidgetImage->getHeight() - bottom; i < height; ++i, ++k) {
+        for (unsigned int i = heightMinusBottom, k = rawHeight - bottom; i < height; ++i, ++k) {
             for (unsigned int j = left * componentsCount, l = 0; j < widthMinusRight * componentsCount; ++j, ++l) {
-                texels[i * width * componentsCount + j] = rawWidgetImage->getTexels()[k * rawWidgetImage->getWidth() * componentsCount + l % ((rawWidgetImage->getWidth() - (left + right)) * componentsCount) + left * componentsCount];
+                texels[i * width * componentsCount + j] = rawTexels[k * rawWidth * componentsCount + l % ((rawWidth - (left + right)) * componentsCount) + left * componentsCount];
             }
         }
 
         //copy corner bottom left
         for (unsigned int i = heightMinusBottom, k = 0; i < height; ++i, ++k) {
             for (unsigned int j = 0; j < leftMultiplyInternalFormat; ++j) {
-                texels[i * width * componentsCount + j] = rawWidgetImage->getTexels()[(k + rawWidgetImage->getHeight() - bottom) * rawWidgetImage->getWidth() * componentsCount + j];
+                texels[i * width * componentsCount + j] = rawTexels[(k + rawHeight - bottom) * rawWidth * componentsCount + j];
             }
         }
 
         //copy left
         for (unsigned int i = top, k = 0;i < heightMinusBottom;++i, ++k) {
             for (unsigned int j = 0; j < leftMultiplyInternalFormat; ++j) {
-                texels[i * width * componentsCount + j] = rawWidgetImage->getTexels()[(k % (rawWidgetImage->getHeight() - (top + bottom)) + top) * rawWidgetImage->getWidth() * componentsCount + j];
+                texels[i * width * componentsCount + j] = rawTexels[(k % (rawHeight - (top + bottom)) + top) * rawWidth * componentsCount + j];
             }
         }
 
         //copy middle
         for (unsigned int i = top, k = 0; i < heightMinusBottom; ++i, ++k) {
             for (unsigned int j = leftMultiplyInternalFormat, l = 0; j < widthMinusRight * componentsCount; ++j, ++l) {
-                texels[i * width * componentsCount + j] = rawWidgetImage->getTexels()[(k % (rawWidgetImage->getHeight() - (top + bottom)) + top) * rawWidgetImage->getWidth() * componentsCount + l%((rawWidgetImage->getWidth() - (left + right)) * componentsCount) + left * componentsCount];
+                texels[i * width * componentsCount + j] = rawTexels[(k % (rawHeight - (top + bottom)) + top) * rawWidth * componentsCount + l % ((rawWidth - (left + right)) * componentsCount) + left * componentsCount];
             }
         }
 
