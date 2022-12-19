f628bb0124d218766bd37a6066f557cf80972821
petitg1987 <petitg1987@gmail.com>
2022-12-19 18:38:00 +0100

    Upgrade release process

--- networkEngine/deps/curl/curl-static.cmake
+++ networkEngine/deps/curl/curl-static.cmake
@@ -12,14 +12,24 @@ set(CURL_CONFIGURE          cd ${CURL_ROOT}/src/curl-static && ./configure CPPFL
 set(CURL_MAKE               cd ${CURL_ROOT}/src/curl-static && make)
 set(CURL_INSTALL            cd ${CURL_ROOT}/src/curl-static && make install)
 
-ExternalProject_Add(curl-static
-        URL                         ${CMAKE_CURRENT_SOURCE_DIR}/deps/curl/curl-7.82.0.zip
-        URL_HASH                    MD5=103a746d6b53f3ab184daa6260add764
-        DOWNLOAD_EXTRACT_TIMESTAMP  true
-        PREFIX                      ${CURL_ROOT}
-        CONFIGURE_COMMAND           ${CURL_CONFIGURE}
-        BUILD_COMMAND               ${CURL_MAKE}
-        INSTALL_COMMAND             ${CURL_INSTALL})
+if(${CMAKE_VERSION} VERSION_LESS "3.24.0") #for build in Docker image
+    ExternalProject_Add(curl-static
+            URL                         ${CMAKE_CURRENT_SOURCE_DIR}/deps/curl/curl-7.82.0.zip
+            URL_HASH                    MD5=103a746d6b53f3ab184daa6260add764
+            PREFIX                      ${CURL_ROOT}
+            CONFIGURE_COMMAND           ${CURL_CONFIGURE}
+            BUILD_COMMAND               ${CURL_MAKE}
+            INSTALL_COMMAND             ${CURL_INSTALL})
+else()
+    ExternalProject_Add(curl-static
+            URL                         ${CMAKE_CURRENT_SOURCE_DIR}/deps/curl/curl-7.82.0.zip
+            URL_HASH                    MD5=103a746d6b53f3ab184daa6260add764
+            DOWNLOAD_EXTRACT_TIMESTAMP  true
+            PREFIX                      ${CURL_ROOT}
+            CONFIGURE_COMMAND           ${CURL_CONFIGURE}
+            BUILD_COMMAND               ${CURL_MAKE}
+            INSTALL_COMMAND             ${CURL_INSTALL})
+endif()
 
 #Dependencies: ${CMAKE_BINARY_DIR}/deps/curl/lib/pkgconfig/libcurl.pc (see Libs.private)
 
