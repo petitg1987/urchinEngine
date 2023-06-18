850c2c96c66ea8a815c755862215ca63c419a75b
petitg1987 <petitg1987@gmail.com>
2023-06-18 22:34:30 +0200

    Compute shader: create generic compute

--- 3dEngine/src/scene/renderer3d/lighting/ambientocclusion/AmbientOcclusionManager.cpp
+++ 3dEngine/src/scene/renderer3d/lighting/ambientocclusion/AmbientOcclusionManager.cpp
@@ -79,7 +79,7 @@ namespace urchin {
             } else {
                 renderTarget = std::make_unique<OffscreenRender>("ambient occlusion", RenderTarget::NO_DEPTH_ATTACHMENT);
             }
-            //TODO output texture on render is probably useless (no need render pass, frame buffer) ?
+            //TODO output texture on render need to be reviewed: no need render pass, neither framebuffer
             static_cast<OffscreenRender*>(renderTarget.get())->addOutputTexture(ambientOcclusionTexture, LoadType::NO_LOAD, std::nullopt, OutputUsage::COMPUTE);
             renderTarget->initialize();
         }
