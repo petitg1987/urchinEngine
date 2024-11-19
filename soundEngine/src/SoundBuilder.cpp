57a34e197041154b13c404478ec5bf29d69ec433
petitg1987 <petitg1987@gmail.com>
2024-11-19 19:28:45 +0100

    Rename sound builder method

--- soundEngine/src/SoundBuilder.cpp
+++ soundEngine/src/SoundBuilder.cpp
@@ -12,54 +12,54 @@ namespace urchin {
 
     }
 
-    std::shared_ptr<SoundComponent> SoundBuilder::newManualEffect(std::string filename, PlayBehavior playBehavior) {
+    std::shared_ptr<SoundComponent> SoundBuilder::newManualTriggerEffect(std::string filename, PlayBehavior playBehavior) {
         auto globalSound = std::make_shared<GlobalSound>(std::move(filename), Sound::SoundCategory::EFFECTS, 1.0f);
         globalSound->preLoadChunks(chunkPreLoader);
         auto manualTrigger = std::make_shared<ManualTrigger>(playBehavior);
         return buildSoundComponent(std::move(globalSound), std::move(manualTrigger));
     }
 
-    std::shared_ptr<SoundComponent> SoundBuilder::newManualMusic(std::string filename, PlayBehavior playBehavior){
+    std::shared_ptr<SoundComponent> SoundBuilder::newManualTriggerMusic(std::string filename, PlayBehavior playBehavior){
         auto globalSound = std::make_shared<GlobalSound>(std::move(filename), Sound::SoundCategory::MUSIC, 1.0f);
         globalSound->preLoadChunks(chunkPreLoader);
         auto manualTrigger = std::make_shared<ManualTrigger>(playBehavior);
         return buildSoundComponent(std::move(globalSound), std::move(manualTrigger));
     }
 
-    std::shared_ptr<SoundComponent> SoundBuilder::newManualSpatialEffect(std::string filename, const Point3<float>& position, float radius, PlayBehavior playBehavior) {
+    std::shared_ptr<SoundComponent> SoundBuilder::newManualTriggerLocalizableEffect(std::string filename, const Point3<float>& position, float radius, PlayBehavior playBehavior) {
         auto localizableSound = std::make_shared<LocalizableSound>(std::move(filename), Sound::SoundCategory::EFFECTS, 1.0f, position, radius);
         localizableSound->preLoadChunks(chunkPreLoader);
         auto manualTrigger = std::make_shared<ManualTrigger>(playBehavior);
         return buildSoundComponent(std::move(localizableSound), std::move(manualTrigger));
     }
 
-    std::shared_ptr<SoundComponent> SoundBuilder::newManualSpatialMusic(std::string filename, const Point3<float>& position, float radius, PlayBehavior playBehavior) {
+    std::shared_ptr<SoundComponent> SoundBuilder::newManualTriggerLocalizableMusic(std::string filename, const Point3<float>& position, float radius, PlayBehavior playBehavior) {
         auto localizableSound = std::make_shared<LocalizableSound>(std::move(filename), Sound::SoundCategory::MUSIC, 1.0f, position, radius);
         localizableSound->preLoadChunks(chunkPreLoader);
         auto manualTrigger = std::make_shared<ManualTrigger>(playBehavior);
         return buildSoundComponent(std::move(localizableSound), std::move(manualTrigger));
     }
 
-    std::shared_ptr<SoundComponent> SoundBuilder::newAreaEffect(std::string filename, std::shared_ptr<AreaTrigger> areaTrigger) {
+    std::shared_ptr<SoundComponent> SoundBuilder::newAreaTriggerEffect(std::string filename, std::shared_ptr<AreaTrigger> areaTrigger) {
         auto globalSound = std::make_shared<GlobalSound>(std::move(filename), Sound::SoundCategory::EFFECTS, 1.0f);
         globalSound->preLoadChunks(chunkPreLoader);
         return buildSoundComponent(std::move(globalSound), std::move(areaTrigger));
     }
 
-    std::shared_ptr<SoundComponent> SoundBuilder::newAreaMusic(std::string filename, std::shared_ptr<AreaTrigger> areaTrigger) {
+    std::shared_ptr<SoundComponent> SoundBuilder::newAreaTriggerMusic(std::string filename, std::shared_ptr<AreaTrigger> areaTrigger) {
         auto globalSound = std::make_shared<GlobalSound>(std::move(filename), Sound::SoundCategory::MUSIC, 1.0f);
         globalSound->preLoadChunks(chunkPreLoader);
         return buildSoundComponent(std::move(globalSound), std::move(areaTrigger));
     }
 
-    std::shared_ptr<SoundComponent> SoundBuilder::newAreaSpatialEffect(std::string filename, const Point3<float>& position, float radius, PlayBehavior playBehavior) {
+    std::shared_ptr<SoundComponent> SoundBuilder::newLocalizableEffect(std::string filename, const Point3<float>& position, float radius, PlayBehavior playBehavior) {
         auto localizableSound = std::make_shared<LocalizableSound>(std::move(filename), Sound::SoundCategory::EFFECTS, 1.0f, position, radius);
         localizableSound->preLoadChunks(chunkPreLoader);
         auto areaTrigger = std::make_shared<AreaTrigger>(playBehavior, std::make_unique<SoundSphere>(radius, position, 0.0f));
         return buildSoundComponent(std::move(localizableSound), std::move(areaTrigger));
     }
 
-    std::shared_ptr<SoundComponent> SoundBuilder::newAreaSpatialMusic(std::string filename, const Point3<float>& position, float radius, PlayBehavior playBehavior) {
+    std::shared_ptr<SoundComponent> SoundBuilder::newLocalizableMusic(std::string filename, const Point3<float>& position, float radius, PlayBehavior playBehavior) {
         auto localizableSound = std::make_shared<LocalizableSound>(std::move(filename), Sound::SoundCategory::MUSIC, 1.0f, position, radius);
         localizableSound->preLoadChunks(chunkPreLoader);
         auto areaTrigger = std::make_shared<AreaTrigger>(playBehavior, std::make_unique<SoundSphere>(radius, position, 0.0f));
--- soundEngine/src/SoundBuilder.h
+++ soundEngine/src/SoundBuilder.h
@@ -13,20 +13,20 @@ namespace urchin {
             explicit SoundBuilder(SoundEnvironment&);
 
             //usage examples: UI sounds, ambient musics
-            std::shared_ptr<SoundComponent> newManualEffect(std::string, PlayBehavior);
-            std::shared_ptr<SoundComponent> newManualMusic(std::string, PlayBehavior);
+            std::shared_ptr<SoundComponent> newManualTriggerEffect(std::string, PlayBehavior);
+            std::shared_ptr<SoundComponent> newManualTriggerMusic(std::string, PlayBehavior);
 
             //usage examples: sound of an opening door, player turn on a music radio
-            std::shared_ptr<SoundComponent> newManualSpatialEffect(std::string, const Point3<float>&, float, PlayBehavior);
-            std::shared_ptr<SoundComponent> newManualSpatialMusic(std::string, const Point3<float>&, float, PlayBehavior);
+            std::shared_ptr<SoundComponent> newManualTriggerLocalizableEffect(std::string, const Point3<float>&, float, PlayBehavior);
+            std::shared_ptr<SoundComponent> newManualTriggerLocalizableMusic(std::string, const Point3<float>&, float, PlayBehavior);
 
             //usage example: user reach end of level and the success sound is played
-            std::shared_ptr<SoundComponent> newAreaEffect(std::string, std::shared_ptr<AreaTrigger>);
-            std::shared_ptr<SoundComponent> newAreaMusic(std::string, std::shared_ptr<AreaTrigger>);
+            std::shared_ptr<SoundComponent> newAreaTriggerEffect(std::string, std::shared_ptr<AreaTrigger>);
+            std::shared_ptr<SoundComponent> newAreaTriggerMusic(std::string, std::shared_ptr<AreaTrigger>);
 
             //usage examples: machine making noise, radio playing musics
-            std::shared_ptr<SoundComponent> newAreaSpatialEffect(std::string, const Point3<float>&, float, PlayBehavior);
-            std::shared_ptr<SoundComponent> newAreaSpatialMusic(std::string, const Point3<float>&, float, PlayBehavior);
+            std::shared_ptr<SoundComponent> newLocalizableEffect(std::string, const Point3<float>&, float, PlayBehavior);
+            std::shared_ptr<SoundComponent> newLocalizableMusic(std::string, const Point3<float>&, float, PlayBehavior);
 
         private:
             std::shared_ptr<SoundComponent> buildSoundComponent(std::shared_ptr<Sound>, std::shared_ptr<SoundTrigger>) const;
