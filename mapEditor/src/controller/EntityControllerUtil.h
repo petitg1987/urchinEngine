211ffb3f4edb013ddd4696a01e7c8635a77e7874
petitg1987 <petitg1987@gmail.com>
2025-02-10 21:48:36 +0100

    Refactor game object

--- mapEditor/src/controller/EntityControllerUtil.h
+++ mapEditor/src/controller/EntityControllerUtil.h
@@ -9,10 +9,11 @@ namespace urchin {
     class EntityControllerUtil {
         public:
             static Point3<float> determineNewPosition(const Point3<float>&, bool, const Camera& camera);
-            static Point3<float> computeFrontCameraPosition(const Camera& camera);
             template<class T> static std::string determineNewCloneName(const std::string&, const std::list<const T*>&);
 
         private:
+            static Point3<float> computeFrontCameraPosition(const Camera& camera);
+
             static constexpr float ENTITY_NEAR_TO_CAMERA_DISTANCE = 50.0f;
     };
 
