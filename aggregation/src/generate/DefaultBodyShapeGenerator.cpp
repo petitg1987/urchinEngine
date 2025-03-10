225b9db3ce01b960b80afb4aa757bbb6878acf86
petitg1987 <petitg1987@gmail.com>
2025-03-10 20:17:55 +0100

    Improve default body shape generator

--- aggregation/src/generate/DefaultBodyShapeGenerator.cpp
+++ aggregation/src/generate/DefaultBodyShapeGenerator.cpp
@@ -168,12 +168,11 @@ namespace urchin {
         Vector3<float> yAxis = cornerPoint.vector(points[orthogonalVectorsToXAxis[0]]);
         Vector3<float> zAxis = cornerPoint.vector(points[orthogonalVectorsToXAxis[1]]);
         Quaternion<float> xOrientation = Quaternion<float>::rotationFromTo(Vector3(1.0f, 0.0f, 0.0f), xAxis.normalize()).normalize();
-        Vector3<float> yAxisRotated = xOrientation.rotateVector(yAxis.normalize());
-        Quaternion<float> yOrientation = Quaternion<float>::rotationFromTo(Vector3(0.0f, 1.0f, 0.0f), yAxisRotated.normalize()).normalize();
+        Quaternion<float> yOrientation = Quaternion<float>::rotationFromTo(xOrientation.rotateVector(Vector3(0.0f, 1.0f, 0.0f)), yAxis.normalize()).normalize();
 
         Vector3 halfSize(xAxis.length() / 2.0f, yAxis.length() / 2.0f, zAxis.length() / 2.0f);
         localizedShape->shape = std::make_unique<const CollisionBoxShape>(halfSize);
-        localizedShape->transform = PhysicsTransform(boxCenterPoint, (xOrientation * yOrientation)); //TODO norm useful ?
+        localizedShape->transform = PhysicsTransform(boxCenterPoint, yOrientation * xOrientation);
         return localizedShape;
     }
 
