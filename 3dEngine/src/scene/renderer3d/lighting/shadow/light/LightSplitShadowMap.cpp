6650beb83f3158b71006ba9a783c52fab77009b2
petitg1987 <petitg1987@gmail.com>
2025-04-18 21:36:32 +0200

    Shadow map for spotlight

--- 3dEngine/src/scene/renderer3d/lighting/shadow/light/LightSplitShadowMap.cpp
+++ 3dEngine/src/scene/renderer3d/lighting/shadow/light/LightSplitShadowMap.cpp
@@ -73,7 +73,7 @@ namespace urchin {
             float tanFov = std::tan(AngleConverter<float>::toRadian(spotLight.getOuterAngle()));
             float nearPlane = 0.15f; //TODO change to 0.05f ? 0.02f ?
             float farPlane = (lightOBBox.getHalfSize(2) * 2.0f) + nearPlane;
-std::cout<<farPlane<<std::endl;
+
             this->lightProjectionMatrix.setValues(
                     1.0f / (tanFov * ratio), 0.0f, 0.0f, 0.0f,
                     0.0f, -1.0f / tanFov, 0.0f, 0.0f,
