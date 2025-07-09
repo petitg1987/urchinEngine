bc114140d4ee0885649117eb251f8377e9d46194
petitg1987 <petitg1987@gmail.com>
2025-07-09 09:33:16 +0200

    Improve shape detector

--- common/src/math/geometry/3d/util/VoxelObjectService.cpp
+++ common/src/math/geometry/3d/util/VoxelObjectService.cpp
@@ -4,11 +4,19 @@
 
 namespace urchin {
 
-    void VoxelObjectService::voxelize(const std::vector<Point3<float>>& vertices, const std::vector<unsigned int>& /*triangleIndices*/) const {
-        AABBox<float> aabbox = computeAABBox(vertices);
-
+    std::size_t VoxelObjectService::VoxelPositionHash::operator()(const Point3<float>& position) const {
+        std::size_t h1 = std::hash<int>()(position.X);
+        std::size_t h2 = std::hash<int>()(position.Y);
+        std::size_t h3 = std::hash<int>()(position.Z);
+        return h1 ^ (h2 << 1) ^ (h3 << 2);
     }
 
+    std::unordered_map<Point3<float>, Point3<float>, VoxelObjectService::VoxelPositionHash> VoxelObjectService::voxelize(const std::vector<Point3<float>>& vertices, const std::vector<unsigned int>& /*triangleIndices*/) const {
+        AABBox<float> aabbox = computeAABBox(vertices);
+        std::unordered_map<Point3<float>, Point3<float>, VoxelPositionHash> voxelMap;
+
+        return voxelMap;
+    }
 
     AABBox<float> VoxelObjectService::computeAABBox(const std::vector<Point3<float>>& vertices) const {
         Point3 min(std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
--- common/src/math/geometry/3d/util/VoxelObjectService.h
+++ common/src/math/geometry/3d/util/VoxelObjectService.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <unordered_map>
 
 #include <math/algebra/point/Point3.h>
 #include <math/geometry/3d/object/AABBox.h>
@@ -9,11 +10,18 @@ namespace urchin {
 
     class VoxelObjectService {
         public:
-            void voxelize(const std::vector<Point3<float>>&, const std::vector<unsigned int>&) const;
+            struct Voxel {
+                Point3<float> position;
+            };
+            struct VoxelPositionHash {
+                std::size_t operator()(const Point3<float>&) const;
+            };
 
+            std::unordered_map<Point3<float>, Point3<float>, VoxelPositionHash> voxelize(const std::vector<Point3<float>>&, const std::vector<unsigned int>&) const;
 
         private:
             AABBox<float> computeAABBox(const std::vector<Point3<float>>&) const;
+
     };
 
 }
