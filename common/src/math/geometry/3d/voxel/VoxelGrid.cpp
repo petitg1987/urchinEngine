8db100a61bcd649ae9b027bcd01131456141291d
petitg1987 <petitg1987@gmail.com>
2025-07-10 18:57:27 +0200

    Improve shape detector

--- common/src/math/geometry/3d/voxel/VoxelGrid.cpp
+++ common/src/math/geometry/3d/voxel/VoxelGrid.cpp
@@ -6,7 +6,7 @@ namespace urchin {
         std::size_t h1 = std::hash<int>()(indexPosition.X);
         std::size_t h2 = std::hash<int>()(indexPosition.Y);
         std::size_t h3 = std::hash<int>()(indexPosition.Z);
-        return h1 ^ (h2 << 1) ^ (h3 << 2);
+        return h1 ^ (h2 << 1) ^ (h3 << 2); //TODO improve speed. Only shift bit without hash ?
     }
 
     VoxelGrid::VoxelGrid(float voxelSize, Point3<float> minCenterPosition) :
