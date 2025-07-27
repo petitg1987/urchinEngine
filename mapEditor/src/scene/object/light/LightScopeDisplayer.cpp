3001d872e308db8a8b518d6d1d938ad073618465
petitg1987 <petitg1987@gmail.com>
2025-07-27 19:23:45 +0200

    Add sun scope

--- mapEditor/src/scene/object/light/LightScopeDisplayer.cpp
+++ mapEditor/src/scene/object/light/LightScopeDisplayer.cpp
@@ -22,30 +22,35 @@ namespace urchin {
             }
             const Light* light = objectEntity->getLight();
 
-            if (const auto* omnidirectionalLight = dynamic_cast<const OmnidirectionalLight*>(light)) {
+            if (const auto* sunLight = dynamic_cast<const SunLight*>(light)) {
+                Point3<float> startPosition = objectEntity->getModel()->getTransform().getPosition();
+                LineSegment3D lineSegment(startPosition, startPosition.translate(sunLight->getDirections()[0].normalize() * 100.0f));
+                auto lineModel = std::make_unique<LineModel>(lineSegment, 1.0f);
+                lineModel->setColor(Vector3(1.0f, 0.0f, 0.0f));
+                lightScopeModels.push_back(std::move(lineModel));
+            } else if (const auto* omnidirectionalLight = dynamic_cast<const OmnidirectionalLight*>(light)) {
                 auto sphereModel = std::make_unique<SphereModel>(omnidirectionalLight->getSphereScope(), 25);
                 sphereModel->setColor(Vector3(1.0f, 0.0f, 0.0f));
-                sphereModel->setPolygonMode(PolygonMode::WIREFRAME);
                 lightScopeModels.push_back(std::move(sphereModel));
 
                 auto boundingBoxModel = std::make_unique<AABBoxModel>(omnidirectionalLight->getAABBox());
                 boundingBoxModel->setColor(Vector3(1.0f, 0.5f, 0.0f));
-                boundingBoxModel->setPolygonMode(PolygonMode::WIREFRAME);
                 lightScopeModels.push_back(std::move(boundingBoxModel));
             } else if (const auto* spotLight = dynamic_cast<const SpotLight*>(light)) {
                 auto coneModel = std::make_unique<ConeModel>(spotLight->getConeScope(), 25);
                 coneModel->setColor(Vector3(1.0f, 0.0f, 0.0f));
-                coneModel->setPolygonMode(PolygonMode::WIREFRAME);
                 lightScopeModels.push_back(std::move(coneModel));
 
                 auto frustumModel = std::make_unique<FrustumModel>(spotLight->getFrustumScope());
                 frustumModel->setColor(Vector3(1.0f, 0.5f, 0.5f));
-                frustumModel->setPolygonMode(PolygonMode::WIREFRAME);
                 lightScopeModels.push_back(std::move(frustumModel));
             }
         }
 
         for (const auto& lightScopeModel : lightScopeModels) {
+            lightScopeModel->setPolygonMode(PolygonMode::WIREFRAME);
+            lightScopeModel->setWireframeLineWidth(0.0015f);
+
             scene.getActiveRenderer3d()->getGeometryContainer().addGeometry(lightScopeModel);
         }
     }
