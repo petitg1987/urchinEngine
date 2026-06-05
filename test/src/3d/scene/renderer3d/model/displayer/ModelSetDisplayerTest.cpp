b665f35642ddb4f19b96d4b3818547af33bcb2e6
petitg1987 <petitg1987@gmail.com>
2026-06-05 18:12:41 +0200

    Add model set displayer test

--- test/src/3d/scene/renderer3d/model/displayer/ModelSetDisplayerTest.cpp
+++ test/src/3d/scene/renderer3d/model/displayer/ModelSetDisplayerTest.cpp
@@ -93,8 +93,8 @@ void ModelSetDisplayerTest::purgeUnusedDisplayer() {
     AssertHelper::assertUnsignedIntEquals(model1->getModelInstanceDisplayers().size(), 0l);
     AssertHelper::assertUnsignedIntEquals(model2->getModelInstanceDisplayers().size(), 1l);
 
-    modelSetDisplayer->~ModelSetDisplayer();
-    modelSetDisplayer.release();
+    //Destroy model set displayer
+    modelSetDisplayer.reset(nullptr);
     AssertHelper::assertUnsignedIntEquals(model1->getObservers(Model::NotificationType::MESH_UV_UPDATED).size(), 0l); //TODO fix it
     AssertHelper::assertUnsignedIntEquals(model2->getObservers(Model::NotificationType::MESH_UV_UPDATED).size(), 0l);
 }
