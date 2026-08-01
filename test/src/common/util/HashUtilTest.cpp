740f6f32fcd53e12f1ebade46172b6d4bf3e5d4f
petitg1987 <petitg1987@gmail.com>
2026-08-01 09:38:04 +0200

    Improve shape detector

--- test/src/common/util/HashUtilTest.cpp
+++ test/src/common/util/HashUtilTest.cpp
@@ -8,8 +8,8 @@ void HashUtilTest::hashBool() {
     std::size_t firstHash = 0;
     std::size_t secondHash = 0;
 
-    HashUtil::combine(firstHash, false, true);
-    HashUtil::combine(secondHash, true, false);
+    HashUtil::hashCombine(firstHash, false, true);
+    HashUtil::hashCombine(secondHash, true, false);
 
     AssertHelper::assertTrue(firstHash != secondHash);
 }
@@ -18,8 +18,8 @@ void HashUtilTest::hashFloat() {
     std::size_t firstHash = 0;
     std::size_t secondHash = 0;
 
-    HashUtil::combine(firstHash, 1.0f, 0.0f);
-    HashUtil::combine(secondHash, 0.0f, 1.0f);
+    HashUtil::hashCombine(firstHash, 1.0f, 0.0f);
+    HashUtil::hashCombine(secondHash, 0.0f, 1.0f);
 
     AssertHelper::assertTrue(firstHash != secondHash);
 }
